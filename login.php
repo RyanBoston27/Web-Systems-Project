08904ce4fff494d773087aa4db6104cbc94eba96
Ryan Boston <ryanboston63@gmail.com>
2022-12-16 17:46:36 -0600

    Login credentials are now checked and added report.html

--- login.php
+++ login.php
@@ -8,25 +8,30 @@ $dbname = "web_systems_final";
 $conn = mysqli_connect($server, $username, $password, $dbname);
 
 if (isset($_POST['submit'])){
-
+    
     if(!empty($_POST['Uname']) && !empty($_POST['pass'])){
-
+        
         $username = $_POST['Uname'];
         $password = $_POST['pass'];
 
-        $query = "SELECT id, Username, Password FROM jobs";
-
+        $query = "SELECT * FROM login WHERE Username = '$username' AND Password = '$password'";
+    
         $run = mysqli_query($conn, $query) or die(mysqli_error($conn));
-
-        header("Location: ./index.html");
-        if($run){
-            header("Location: ./index.html");
+        $check = mysqli_fetch_array($run);
+        if(isset($check)){
+            ?>
+           <script type="text/javascript">
+           alert("Successfully logged in!");
+           window.location.href = "index.html";
+           </script>
+           <?php
         }
         else{
-            echo "Form not submitted";
+            echo '<script>alert("Invalid Login Details")</script>';
         }
     }
     else{
         echo "All Fields Required";
     }
 }
+?>
